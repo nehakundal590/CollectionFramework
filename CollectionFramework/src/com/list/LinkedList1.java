d2259c785e2f798714336c0b8a515ceeb656ceeb
neha <devineha590@gmail.com>
2026-02-07 00:20:10 +0530

    collection framework

---
+++ CollectionFramework/src/com/list/LinkedList1.java
@@ -0,0 +1,17 @@
+package com.list;
+
+import java.util.LinkedList;
+
+public class LinkedList1 {
+	public static void main(String[] args) {
+		LinkedList l1=new LinkedList();
+		l1.add(10);
+		l1.add(20);
+		l1.add(30);
+		l1.add(30);
+		l1.add(null);
+		l1.add(null);
+		System.out.println(l1);
+	}
+
+}
